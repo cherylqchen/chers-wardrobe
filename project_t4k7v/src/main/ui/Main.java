98936f4ba5a434b611bf501744f1162ba461f37b
Cheryl Chen <49375661+cherylqchen@users.noreply.github.com>
2023-12-30 14:54:21 -0800

    Update Main.java to make the event log compatible with command-Q.

--- project_t4k7v/src/main/ui/Main.java
+++ project_t4k7v/src/main/ui/Main.java
@@ -6,6 +6,7 @@ public class Main {
     // EFFECTS: run the GUI when given new WardrobeGUI();
     //          will run console-based app if given new ChersWardrobeApp();
     public static void main(String[] args) {
+        System.setProperty("apple.eawt.quitStrategy", "CLOSE_ALL_WINDOWS");
         new WardrobeGUI();
 
         // for console based app:
